9b5f71fa03e699ee11503fe82e40940e8d1a8bec
FortB164 <94996894+FortB164@users.noreply.github.com>
2025-03-06 15:34:11 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,3 +1,11 @@
+@echo off
+openfiles >nul 2>nul
+if %errorlevel% NEQ 0 (
+    echo Requesting Administrator privileges...
+    powershell -Command "Start-Process cmd -ArgumentList '/c %~s0' -Verb runAs"
+    exit /b
+)
+
 py -m pip install psutil pyinstaller pywin32 collections
 py SMS.py install
 py SMS.py start
